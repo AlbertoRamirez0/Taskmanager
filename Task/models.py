b555abb77cd262a181163dbece08f6eba1a1a90e
Alexi Ramirez <rf0196022023@unab.edu.sv>
2025-08-28 09:13:08 -0600

    primer commit

---
+++ Task/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=50 , default='Pending')
+    created_at = models.DateTimeField(datetime.now)
