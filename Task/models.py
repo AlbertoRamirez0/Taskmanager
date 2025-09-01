587b6a276d40b31b26c1cc666398da8a1a44d7aa
Alexi Ramirez <rf0196022023@unab.edu.sv>
2025-09-01 10:07:48 -0600

    cambios en model createat

--- Task/models.py
+++ Task/models.py
@@ -6,4 +6,4 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     status = models.CharField(max_length=50 , default='Pending')
-    created_at = models.DateTimeField(datetime.now)
+    created_at = models.DateTimeField(default = datetime.now)
